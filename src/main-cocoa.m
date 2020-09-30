f57f345e7274f156600d70176c49066e0a906212
Eric Branlund <ebranlund@fastmail.com>
2020-09-29 19:09:06 -0700

    Don't bother to call term_activate() in drawRect() since the contents of Term are not used.  Was observed to prevent a crash at start up in Angband 4.2.1 on Mac OS X 10.12.6.

--- src/main-cocoa.m
+++ src/main-cocoa.m
@@ -3312,7 +3312,6 @@ static int compare_nsrect_yorigin_greater(const void *ap, const void *bp)
     NSGraphicsContext *nsctx = nil;
     CGContextRef ctx = 0;
     NSFont* screenFont = nil;
-    term_type *old = 0;
     int graf_width = 0, graf_height = 0;
     int overdraw_row = 0, overdraw_max = 0;
     wchar_t blank = 0;
@@ -3320,8 +3319,6 @@ static int compare_nsrect_yorigin_greater(const void *ap, const void *bp)
 	rect.origin.x + rect.size.width > self.borderSize.width &&
 	rect.origin.y < bottomY &&
 	rect.origin.y + rect.size.height > self.borderSize.height) {
-	old = Term;
-	term_activate(self->terminal);
 	nsctx = [NSGraphicsContext currentContext];
 	ctx = [nsctx graphicsPort];
 	screenFont = [self.angbandViewFont screenFont];
@@ -3347,9 +3344,6 @@ static int compare_nsrect_yorigin_greater(const void *ap, const void *bp)
     if (overdraw_row && invalidCount > 1) {
 	sortedRects = malloc(invalidCount * sizeof(NSRect));
 	if (sortedRects == 0) {
-	    if (old != 0) {
-		term_activate(old);
-	    }
 	    NSException *exc = [NSException exceptionWithName:@"OutOfMemory"
 					    reason:@"sorted rects in drawRect"
 					    userInfo:nil];
@@ -3696,9 +3690,6 @@ static int compare_nsrect_yorigin_greater(const void *ap, const void *bp)
     }
 
     free(sortedRects);
-    if (old != 0) {
-	term_activate(old);
-    }
 }
 
 - (BOOL)isOrderedIn
